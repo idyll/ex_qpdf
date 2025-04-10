2186a0d1f4f9981d4d7f62f6c697fbf72e2af42a
Mark Madsen <mm@idyll.io>
2025-04-10 13:24:53 -0600

    github actions

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExQPDF.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/idyll/ex_qpdf"
   @description """
   An Elixir wrapper for the QPDF library, providing a simple interface for working with PDF files.
